96790ff710988328ef0bbe88b762993aebafdb31
vmqxdv <vquintilio@gmail.com>
2025-09-14 10:57:59 +0200

    first commit

---
+++ src/test/java/org/lessons/java/thymeleaf/best_of_the_year/BestOfTheYearApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lessons.java.thymeleaf.best_of_the_year;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BestOfTheYearApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
